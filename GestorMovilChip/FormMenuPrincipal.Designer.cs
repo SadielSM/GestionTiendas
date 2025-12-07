fc795bf202797e13851e214e3d629defd675f41f
SadielSM <sadielsm56@gmail.com>
2025-12-07 20:45:41 +0100

    Editado Botón

--- GestorMovilChip/FormMenuPrincipal.Designer.cs
+++ GestorMovilChip/FormMenuPrincipal.Designer.cs
@@ -40,6 +40,7 @@
             this.lblVentasMes = new System.Windows.Forms.Label();
             this.lblTituloStockBajo = new System.Windows.Forms.Label();
             this.lblStockBajo = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // lblBienvenida
@@ -157,11 +158,21 @@
             this.lblStockBajo.TabIndex = 11;
             this.lblStockBajo.Text = "0.00€";
             // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(596, 249);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(118, 47);
+            this.button1.TabIndex = 12;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
             // FormMenuPrincipal
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1336, 594);
+            this.Controls.Add(this.button1);
             this.Controls.Add(this.lblStockBajo);
             this.Controls.Add(this.lblTituloStockBajo);
             this.Controls.Add(this.lblVentasMes);
@@ -200,5 +211,6 @@
         private System.Windows.Forms.Label lblVentasMes;
         private System.Windows.Forms.Label lblTituloStockBajo;
         private System.Windows.Forms.Label lblStockBajo;
+        private System.Windows.Forms.Button button1;
     }
 }
